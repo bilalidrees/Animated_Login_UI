6f232a8cb60f113116770d5b041ff400c610afd3
bilalidrees <bilalidreesbilal2@gmail.com>
2022-01-06 15:53:04 +0500

    release commit

---
+++ Animated-Login-Page-UI-main/README.md
@@ -0,0 +1,9 @@
+# Animated-Login-Page-UI
+
+
+
+
+
+
+
+![login page ui gif file](https://user-images.githubusercontent.com/58117224/122635830-a2b7e580-d103-11eb-8e2d-485480a24a46.gif)
---
+++ lib/main.dart
@@ -0,0 +1,347 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'dart:ui';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() async {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      title: "Animated-Login-Page-UI",
+    ),
+  );
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController controller1;
+  AnimationController controller2;
+  Animation<double> animation1;
+  Animation<double> animation2;
+  Animation<double> animation3;
+  Animation<double> animation4;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller1 = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 5,
+      ),
+    );
+    animation1 = Tween<double>(begin: .1, end: .15).animate(
+      CurvedAnimation(
+        parent: controller1,
+        curve: Curves.easeInOut,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller1.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller1.forward();
+        }
+      });
+    animation2 = Tween<double>(begin: .02, end: .04).animate(
+      CurvedAnimation(
+        parent: controller1,
+        curve: Curves.easeInOut,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    controller2 = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 5,
+      ),
+    );
+    animation3 = Tween<double>(begin: .41, end: .38).animate(CurvedAnimation(
+      parent: controller2,
+      curve: Curves.easeInOut,
+    ))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller2.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller2.forward();
+        }
+      });
+    animation4 = Tween<double>(begin: 170, end: 190).animate(
+      CurvedAnimation(
+        parent: controller2,
+        curve: Curves.easeInOut,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    Timer(Duration(milliseconds: 2500), () {
+      controller1.forward();
+    });
+
+    controller2.forward();
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    controller2.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xff192028),
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: size.height * (animation2.value + .58),
+                  left: size.width * .21,
+                  child: CustomPaint(
+                    painter: MyPainter(50),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .98,
+                  left: size.width * .1,
+                  child: CustomPaint(
+                    painter: MyPainter(animation4.value - 30),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .5,
+                  left: size.width * (animation2.value + .8),
+                  child: CustomPaint(
+                    painter: MyPainter(30),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * animation3.value,
+                  left: size.width * (animation1.value + .1),
+                  child: CustomPaint(
+                    painter: MyPainter(60),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * .1,
+                  left: size.width * .8,
+                  child: CustomPaint(
+                    painter: MyPainter(animation4.value),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Padding(
+                        padding: EdgeInsets.only(top: size.height * .1),
+                        child: Text(
+                          'APP NAME',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(.7),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            wordSpacing: 4,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          component1(Icons.account_circle_outlined,
+                              'User name...', false, false),
+                          component1(
+                              Icons.email_outlined, 'Email...', false, true),
+                          component1(
+                              Icons.lock_outline, 'Password...', true, false),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              component2(
+                                'LOGIN',
+                                2.58,
+                                () {
+                                  HapticFeedback.lightImpact();
+                                  Fluttertoast.showToast(
+                                      msg: 'Login button pressed');
+                                },
+                              ),
+                              SizedBox(width: size.width / 20),
+                              component2(
+                                'Forgotten password!',
+                                2.58,
+                                () {
+                                  HapticFeedback.lightImpact();
+                                  Fluttertoast.showToast(
+                                      msg: 'Forgotten password button pressed');
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          component2(
+                            'Create a new Account',
+                            2,
+                            () {
+                              HapticFeedback.lightImpact();
+                              Fluttertoast.showToast(
+                                  msg: 'Create a new account button pressed');
+                            },
+                          ),
+                          SizedBox(height: size.height * .05),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component1(
+      IconData icon, String hintText, bool isPassword, bool isEmail) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaY: 15,
+          sigmaX: 15,
+        ),
+        child: Container(
+          height: size.width / 8,
+          width: size.width / 1.2,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(right: size.width / 30),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(.05),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: TextField(
+            style: TextStyle(color: Colors.white.withOpacity(.8)),
+            cursorColor: Colors.white,
+            obscureText: isPassword,
+            keyboardType:
+                isEmail ? TextInputType.emailAddress : TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.white.withOpacity(.7),
+              ),
+              border: InputBorder.none,
+              hintMaxLines: 1,
+              hintText: hintText,
+              hintStyle:
+                  TextStyle(fontSize: 14, color: Colors.white.withOpacity(.5)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component2(String string, double width, VoidCallback voidCallback) {
+    Size size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaY: 15, sigmaX: 15),
+        child: InkWell(
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onTap: voidCallback,
+          child: Container(
+            height: size.width / 8,
+            width: size.width / width,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(.05),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text(
+              string,
+              style: TextStyle(color: Colors.white.withOpacity(.8)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final double radius;
+
+  MyPainter(this.radius);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..shader = LinearGradient(
+              colors: [Color(0xffFD5E3D), Color(0xffC43990)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)
+          .createShader(Rect.fromCircle(
+        center: Offset(0, 0),
+        radius: radius,
+      ));
+
+    canvas.drawCircle(Offset.zero, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
